eee753bc0b8bf0f5a47f022e84611dfe8490461c
joseph.lizier <joseph.lizier@gmail.com>
2012-08-01 12:14:24 +0000

    Adding unit tests for Chi Square distribution in MathsUtils and for Entropy Calculator Gaussian

---
+++ java/unittests/infodynamics/measures/continuous/gaussian/EntropyCalculatorLinearGaussianTest.java
@@ -0,0 +1,36 @@
+package infodynamics.measures.continuous.gaussian;
+
+import infodynamics.utils.MatrixUtils;
+import infodynamics.utils.RandomGenerator;
+import junit.framework.TestCase;
+
+public class EntropyCalculatorLinearGaussianTest extends TestCase {
+
+	public void testVarianceSetting() {
+		EntropyCalculatorGaussian entcalc = new EntropyCalculatorGaussian();
+		entcalc.initialise();
+		double variance = 3.45567;
+		entcalc.setVariance(variance);
+		assertEquals(variance, entcalc.variance);
+	}
+
+	public void testVarianceCalculation() {
+		EntropyCalculatorGaussian entcalc = new EntropyCalculatorGaussian();
+		entcalc.initialise();
+		RandomGenerator randomGenerator = new RandomGenerator();
+		double[] gaussianObservations = randomGenerator.generateNormalData(100, 5, 3);
+		double variance = MatrixUtils.stdDev(gaussianObservations);
+		variance *= variance;
+		entcalc.setObservations(gaussianObservations);
+		assertEquals(variance, entcalc.variance);
+	}
+	
+	public void testEntropyCalculation() {
+		EntropyCalculatorGaussian entcalc = new EntropyCalculatorGaussian();
+		entcalc.initialise();
+		double variance = 3.45567;
+		entcalc.setVariance(variance);
+		double expectedEntropy = 0.5 * Math.log(2.0*Math.PI*Math.E*variance);
+		assertEquals(expectedEntropy, entcalc.computeAverageLocalOfObservations());
+	}
+}
