8f6c29e301c8693626d2c50123137d8a303c49d3
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2012-10-25 01:14:55 +0000

    Added AnalyticNullDistributionComputer and refactored the reference to it in MutualInfoCalculatorMultivariateGaussian

---
+++ trunk/java/source/infodynamics/utils/AnalyticNullDistributionComputer.java
@@ -0,0 +1,15 @@
+package infodynamics.utils;
+
+
+/**
+ * Calculators implementing this interface must provide a method to compute
+ *  the statistical significance of their measurement, returning an analytically
+ *  determined distribution.
+ * 
+ * @author Joseph Lizier
+ *
+ */
+public interface AnalyticNullDistributionComputer {
+
+	public AnalyticMeasurementDistribution computeSignificance();
+}
