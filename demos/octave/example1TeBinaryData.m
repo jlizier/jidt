bb969f7d2cf7c6e12d26c51424cb778dee0c4da1
joseph.lizier <joseph.lizier@gmail.com>
2012-10-26 06:34:59 +0000

    Adding basic Octave/Matlab demo scripts

---
+++ demos/octave/example1TeBinaryData.m
@@ -0,0 +1,25 @@
+% Example 1 - Transfer entropy on binary data =
+
+% Simple transfer entropy (TE) calculation on binary data using the discrete TE calculator:
+
+% Change location of jar to match yours:
+javaaddpath('../../infodynamics.jar');
+
+% Generate some random binary data.
+% Note that we need the *1 to make this a number not a Boolean,
+%  otherwise this will not work (as it cannot match the method signature)
+sourceArray=(rand(100,1)>0.5)*1; 
+destArray = [0; sourceArray(1:99)];
+sourceArray2=(rand(100,1)>0.5)*1;
+% Create a TE calculator and run it:
+teCalc=javaObject('infodynamics.measures.discrete.ApparentTransferEntropyCalculator', 2, 1);
+teCalc.initialise();
+% Since we have simple arrays of doubles, we can directly pass these in:
+teCalc.addObservations(destArray, sourceArray);
+printf("For copied source, result should be close to 1 bit : ");
+result = teCalc.computeAverageLocalOfObservations()
+teCalc.initialise();
+teCalc.addObservations(destArray, sourceArray2);
+printf("For random source, result should be close to 0 bits: ");
+result2 = teCalc.computeAverageLocalOfObservations()
+
