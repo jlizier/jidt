c2ac59c310a552d312b83b99b404c87ca8da5f84
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2013-01-14 12:49:16 +0000

    Added first python code example using JIDT

---
+++ trunk/demos/python/example1TeBinaryData.py
@@ -0,0 +1,30 @@
+# Example 1 - Transfer entropy on binary data =
+
+# Simple transfer entropy (TE) calculation on binary data using the discrete TE calculator:
+
+from jpype import *
+import random
+
+# Change location of jar to match yours:
+jarLocation = "../../infodynamics.jar"
+# Start the JVM (add the "-Xmx" option with say 1024M if you get crashes due to not enough memory space)
+startJVM(getDefaultJVMPath(), "-ea", "-Djava.class.path=" + jarLocation)
+
+# Generate some random binary data.
+sourceArray = [random.randint(0,1) for r in xrange(100)]
+destArray = [0] + sourceArray[0:99];
+sourceArray2 = [random.randint(0,1) for r in xrange(100)]
+
+# Create a TE calculator and run it:
+teCalcClass = JPackage("infodynamics.measures.discrete").ApparentTransferEntropyCalculator
+teCalc = teCalcClass(2,1)
+teCalc.initialise()
+# Since we have simple arrays of doubles, we can directly pass these in:
+teCalc.addObservations(destArray, sourceArray)
+print("For copied source, result should be close to 1 bit : %.4f" % teCalc.computeAverageLocalOfObservations())
+teCalc.initialise()
+teCalc.addObservations(destArray, sourceArray2)
+print("For random source, result should be close to 0 bits: %.4f" % teCalc.computeAverageLocalOfObservations())
+
+shutdownJVM() 
+
