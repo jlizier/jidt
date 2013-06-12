06fa1567ab01ff4a8be2f454818d58807776cd6c
joseph.lizier <joseph.lizier@gmail.com>
2013-06-12 00:33:09 +0000

    Adding Java demo examples 1 and 2

---
+++ demos/java/infodynamics/demos/Example1TeBinaryData.java
@@ -0,0 +1,45 @@
+package infodynamics.demos;
+
+import infodynamics.utils.RandomGenerator;
+import infodynamics.measures.discrete.ApparentTransferEntropyCalculator;
+
+/**
+ * 
+ * = Example 1 - Transfer entropy on binary data =
+ * 
+ * Simple transfer entropy (TE) calculation on binary data using the discrete TE calculator.
+ * 
+ * @author Joseph Lizier
+ *
+ */
+public class Example1TeBinaryData {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int arrayLengths = 100;
+		RandomGenerator rg = new RandomGenerator();
+		
+		// Generate some random binary data:
+		int[] sourceArray = rg.generateRandomInts(arrayLengths, 2);
+		int[] destArray = new int[arrayLengths];
+		destArray[0] = 0;
+		System.arraycopy(sourceArray, 0, destArray, 1, arrayLengths - 1);
+		int[] sourceArray2 = rg.generateRandomInts(arrayLengths, 2);
+		
+		// Create a TE calculator and run it:
+		ApparentTransferEntropyCalculator teCalc=
+				new ApparentTransferEntropyCalculator(2, 1);
+		teCalc.initialise();
+		teCalc.addObservations(destArray, sourceArray);
+		double result = teCalc.computeAverageLocalOfObservations();
+		System.out.printf("For copied source, result should be close to 1 bit : %.3f bits\n", result);
+		teCalc.initialise();
+		teCalc.addObservations(destArray, sourceArray2);
+		double result2 = teCalc.computeAverageLocalOfObservations();
+		System.out.printf("For random source, result should be close to 0 bits: %.3f bits\n", result2);
+	}
+
+}
---
+++ demos/java/infodynamics/demos/Example2TeMultidimBinaryData.java
@@ -0,0 +1,51 @@
+package infodynamics.demos;
+
+import infodynamics.utils.RandomGenerator;
+import infodynamics.measures.discrete.ApparentTransferEntropyCalculator;
+
+/**
+ * 
+ * = Example 2 - Transfer entropy on multidimensional binary data =
+ * 
+ * Simple transfer entropy (TE) calculation on multidimensional binary data using the discrete TE calculator.
+ *
+ * This example shows how to handle multidimensional arrays where
+ *  we pool the observations over all variables with the discrete calculator.
+ * 
+ * @author Joseph Lizier
+ *
+ */
+public class Example2TeMultidimBinaryData {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int timeSteps = 2;
+		int variables = 100;
+		RandomGenerator rg = new RandomGenerator();
+		
+		// Create many columns in a multidimensional array (2 rows by 100 columns),
+		//  where the next time step (row 2) copies the value of the column on the left
+		//  from the previous time step (row 1):
+		int[][] twoDTimeSeries = new int[timeSteps][];
+		twoDTimeSeries[0] = rg.generateRandomInts(variables, 2);
+		twoDTimeSeries[1] = new int[variables];
+		twoDTimeSeries[1][0] = twoDTimeSeries[0][variables - 1];
+		System.arraycopy(twoDTimeSeries[0], 0, twoDTimeSeries[1], 1, variables - 1);
+
+		// Create a TE calculator and run it:
+		ApparentTransferEntropyCalculator teCalc=
+				new ApparentTransferEntropyCalculator(2, 1);
+		teCalc.initialise();
+		// Add observations of transfer across one cell to the right (j=1)
+		//  per time step:
+		teCalc.addObservations(twoDTimeSeries, 1);
+
+		double result2D = teCalc.computeAverageLocalOfObservations();
+		System.out.printf("The result should be close to 1 bit here, " +
+				"since we are executing copy operations of what is effectively " +
+				"a random bit to each cell here: %.3f bits\n", result2D);
+	}
+}
