e28c40bb903c9a6ef56e31564a0d7eda83c3d079
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2013-10-22 04:06:58 +0000

    Updating unit tests to reflect changes to discrete MI calculator and method of generating random permutations.

--- trunk/java/unittests/infodynamics/measures/discrete/ConditionalMutualInformationTester.java
+++ trunk/java/unittests/infodynamics/measures/discrete/ConditionalMutualInformationTester.java
@@ -1,5 +1,6 @@
 package infodynamics.measures.discrete;
 
+import infodynamics.utils.MatrixUtils;
 import infodynamics.utils.RandomGenerator;
 
 import junit.framework.TestCase;
@@ -20,6 +21,22 @@ public class ConditionalMutualInformationTester extends TestCase {
 		condMiCalc.addObservations(x1, x2, cond);
 		condMiCalc.computeAverageLocalOfObservations();
 		condMiCalc.computeSignificance(1000);
+	}
+
+	public void testComputeLocalsGivesCorrectAverage() {
+		ConditionalMutualInformationCalculator condMiCalc = new ConditionalMutualInformationCalculator(2, 2, 2);
+		int[] x1 = rand.generateRandomInts(numObservations, 2);
+		int[] x2 = rand.generateRandomInts(numObservations, 2);
+		int[] cond = rand.generateRandomInts(numObservations, 2);
+		condMiCalc.initialise();
+		condMiCalc.addObservations(x1, x2, cond);
+		double average = condMiCalc.computeAverageLocalOfObservations();
+		double avLastAverage = condMiCalc.getLastAverage();
+		double[] locals = condMiCalc.computeLocal(x1, x2, cond);
+		double localsLastAverage = condMiCalc.getLastAverage();
+		assertEquals(average, MatrixUtils.mean(locals), 0.000001);
+		assertEquals(avLastAverage, localsLastAverage, 0.000001);
+		assertEquals(average, avLastAverage, 0.000001);
 	}
 
 	public void testSetDebug() {
