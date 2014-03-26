57da6a9d4ca000d7e316325384c373053ebd31b9
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2014-03-26 05:08:46 +0000

    Moved Kraskov mutual info calculator multivariate to inherit from the common MI multivariate class, so as to share code for adding observations, statistical tests, etc. Passed unit testing.

--- trunk/java/source/infodynamics/measures/continuous/kraskov/MutualInfoCalculatorMultiVariateKraskov.java
+++ trunk/java/source/infodynamics/measures/continuous/kraskov/MutualInfoCalculatorMultiVariateKraskov.java
@@ -1,33 +1,54 @@
 package infodynamics.measures.continuous.kraskov;
 
 import infodynamics.measures.continuous.MutualInfoCalculatorMultiVariate;
+import infodynamics.measures.continuous.MutualInfoMultiVariateCommon;
 import infodynamics.utils.EuclideanUtils;
 import infodynamics.utils.MatrixUtils;
-import infodynamics.utils.EmpiricalMeasurementDistribution;
-import infodynamics.utils.RandomGenerator;
 
 /**
- * <p>Compute the Mutual Information between two vectors using the Kraskov estimation method.</p>
- * <p>Computes this directly looking at the marginal space for each variable, rather than
- * using the multi-info (or integration) in the marginal spaces.
- * Two child classes actually implement the two algorithms in the Kraskov paper.</p>
- * @see "Estimating mutual information", Kraskov, A., Stogbauer, H., Grassberger, P., Physical Review E 69, (2004) 066138
- * @see http://dx.doi.org/10.1103/PhysRevE.69.066138
+ * <p>Computes the differential mutual information of two given multivariate sets of
+ *  observations,
+ *  using Kraskov-Grassberger estimation (see Kraskov et al., below).
+ *  Two child classes {@link MutualInfoCalculatorMultiVariateKraskov1} and
+ *  {@link MutualInfoCalculatorMultiVariateKraskov2}
+ *  actually implement the two algorithms in the Kraskov et al. paper</p>
+ *  
+ * <p>
+ * Usage:
+ * 	<ol>
+ * 		<li>Construct see {@link MutualInfoCalculatorMultiVariateKraskov1#MutualInfoCalculatorMultiVariateKraskov1} </li>
+ * 		<li>Set properties using {@link #setProperty(String, String)}</li>
+ *		<li>Initialise: {@link #initialise(int, int)}</li>
+ * 		<li>Provide the observations to the calculator using:
+ * 			{@link #setObservations(double[][], double[][])}
+ * 			a sequence of:
+ * 			{@link #startAddObservations()},
+ *          multiple calls to either {@link #addObservations(double[][], double[][])}
+ *          or {@link #addObservations(double[][], double[][], int, int)}, and then
+ *          {@link #finaliseAddObservations()}.</li>
+ * 		<li>Compute the required information-theoretic results, primarily:
+ * 			{@link #computeAverageLocalOfObservations()} to return the average MI
+ *          entropy based on the supplied observations; or other calls to compute
+ *          local values or statistical significance.</li>
+ * 	</ol>
+ * </p>
  * 
- * @author Joseph Lizier
+ * <p>
+ * TODO Add fast nearest neighbour searches to the child classes
+ * </p>
+ * 
+ * @see "Estimating mutual information", Kraskov, A., Stogbauer, H., Grassberger, P., Physical Review E 69, (2004) 066138
+ * @see {@link http://dx.doi.org/10.1103/PhysRevE.69.066138}
+ * @author Joseph Lizier, <a href="mailto:joseph.lizier at gmail.com">joseph.lizier at gmail.com</a>
  */
-public abstract class MutualInfoCalculatorMultiVariateKraskov implements
-		MutualInfoCalculatorMultiVariate {
+public abstract class MutualInfoCalculatorMultiVariateKraskov
+	extends MutualInfoMultiVariateCommon
+	implements MutualInfoCalculatorMultiVariate {
 
 	/**
 	 * we compute distances to the kth neighbour
 	 */
 	protected int k;
-	protected double[][] data1;
-	protected double[][] data2;
-	protected boolean debug;
-	protected double mi;
-	protected boolean miComputed;
 	
 	protected EuclideanUtils normCalculator;
 	// Storage for the norms from each observation to each other one
@@ -42,9 +63,6 @@ public abstract class MutualInfoCalculatorMultiVariateKraskov implements
 	public final static String PROP_NORM_TYPE = "NORM_TYPE";
 	public static final String PROP_NORMALISE = "NORMALISE";
 	private boolean normalise = true;
-	private int timeDiff = 0;
-
-	protected boolean addedMoreThanOneObservationSet = false;
 
 	public MutualInfoCalculatorMultiVariateKraskov() {
 		super();
@@ -52,14 +70,10 @@ public abstract class MutualInfoCalculatorMultiVariateKraskov implements
 		normCalculator = new EuclideanUtils(EuclideanUtils.NORM_MAX_NORM);
 	}
 
-	public void initialise(int dimensions1, int dimensions2) {
-		mi = 0.0;
-		miComputed = false;
+	public void initialise(int sourceDimensions, int destDimensions) {
+		super.initialise(sourceDimensions, destDimensions);
 		xNorms = null;
 		yNorms = null;
-		data1 = null;
-		data2 = null;
-		// No need to keep the dimenions here
 	}
 
 	/**
@@ -73,81 +87,45 @@ public abstract class MutualInfoCalculatorMultiVariateKraskov implements
 	 * 		default is {@link EuclideanUtils#NORM_MAX_NORM}.
 	 *  <li>{@link #PROP_NORMALISE} - whether to normalise the individual
 	 *      variables (true by default)</li>
-	 *  <li>{@link MutualInfoCalculatorMultiVariate#PROP_TIME_DIFF}</li>
 	 * </ul>
+	 * or any properties set in {@link MutualInfoMultiVariateCommon#setProperty(String, String)}.
 	 * 
 	 * @param propertyName
 	 * @param propertyValue
 	 */
-	public void setProperty(String propertyName, String propertyValue) {
+	public void setProperty(String propertyName, String propertyValue) throws Exception {
+		boolean propertySet = true;
 		if (propertyName.equalsIgnoreCase(PROP_K)) {
 			k = Integer.parseInt(propertyValue);
 		} else if (propertyName.equalsIgnoreCase(PROP_NORM_TYPE)) {
 			normCalculator.setNormToUse(propertyValue);
 		} else if (propertyName.equalsIgnoreCase(PROP_NORMALISE)) {
 			normalise = Boolean.parseBoolean(propertyValue);
-		} else if (propertyName.equalsIgnoreCase(PROP_TIME_DIFF)) {
-			timeDiff = Integer.parseInt(propertyValue);
-			if (timeDiff < 0) {
-				throw new RuntimeException("Time difference must be >= 0. Flip data1 and data2 around if required.");
-			}
-		}
-	}
-
-	public void addObservations(double[][] source, double[][] destination) throws Exception {
-		throw new RuntimeException("Not implemented yet");
-	}
-
-	public void addObservations(double[][] source, double[][] destination, int startTime, int numTimeSteps) throws Exception {
-		throw new RuntimeException("Not implemented yet");
-	}
-
-	public void setObservations(double[][] source, double[][] destination, boolean[] sourceValid, boolean[] destValid) throws Exception {
-		// Will need to handle timeDiff here
-		throw new RuntimeException("Not implemented yet");
-	}
-
-	public void setObservations(double[][] source, double[][] destination, boolean[][] sourceValid, boolean[][] destValid) throws Exception {
-		// Will need to handle timeDiff here
-		throw new RuntimeException("Not implemented yet");
-	}
-
-	public void startAddObservations() {
-		throw new RuntimeException("Not implemented yet");
-	}
-
-	public void finaliseAddObservations() {
-		throw new RuntimeException("Not implemented yet");
-	}
-
-	public void setObservations(double[][] observations1,
-			double[][] observations2) throws Exception {
-		if (observations1.length != observations2.length) {
-			throw new Exception("Time steps for observations2 " +
-					observations2.length + " does not match the length " +
-					"of observations1 " + observations1.length);
-		}
-		if ((observations1[0].length == 0) || (observations2[0].length == 0)) {
-			throw new Exception("Computing MI with a null set of data");
-		}
-		// Grab the data
-		if (timeDiff == 0) {
-			data1 = observations1;
-			data2 = observations2;
 		} else {
-			// MI with time difference: I(x_{1,n}; x_{2,n+timeDiff})
-			if (observations1.length < timeDiff) {
-				throw new Exception("Computing MI with too few observations " +
-						observations1.length + " given time diff " + timeDiff);
-			}
-			data1 = MatrixUtils.selectRows(observations1, 0, observations1.length - timeDiff);
-			data2 = MatrixUtils.selectRows(observations2, timeDiff, observations2.length - timeDiff);
+			// No property was set here
+			propertySet = false;
+			// try the superclass:
+			super.setProperty(propertyName, propertyValue);
 		}
-		// Normalise it if required
+		if (debug && propertySet) {
+			System.out.println(this.getClass().getSimpleName() + ": Set property " + propertyName +
+					" to " + propertyValue);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see infodynamics.measures.continuous.MutualInfoMultiVariateCommon#finaliseAddObservations()
+	 */
+	@Override
+	public void finaliseAddObservations() throws Exception {
+		// Allow the parent to generate the data for us first
+		super.finaliseAddObservations();
+		
+		// Then normalise the data if required
 		if (normalise) {
 			// Take a copy since we're going to normalise it
-			data1 = MatrixUtils.normaliseIntoNewArray(data1);
-			data2 = MatrixUtils.normaliseIntoNewArray(data2);
+			sourceObservations = MatrixUtils.normaliseIntoNewArray(sourceObservations);
+			destObservations = MatrixUtils.normaliseIntoNewArray(destObservations);
 		}
 	}
 
@@ -156,13 +134,13 @@ public abstract class MutualInfoCalculatorMultiVariateKraskov implements
 	 *
 	 */
 	protected void computeNorms() {
-		int N = data1.length; // number of observations
+		int N = sourceObservations.length; // number of observations
 		
 		xNorms = new double[N][N];
 		yNorms = new double[N][N];
 		for (int t = 0; t < N; t++) {
 			// Compute the norms from t to all other time points
-			double[][] xyNormsForT = normCalculator.computeNorms(data1, data2, t);
+			double[][] xyNormsForT = normCalculator.computeNorms(sourceObservations, destObservations, t);
 			for (int t2 = 0; t2 < N; t2++) {
 				xNorms[t][t2] = xyNormsForT[t2][0];
 				yNorms[t][t2] = xyNormsForT[t2][1];
@@ -184,70 +162,6 @@ public abstract class MutualInfoCalculatorMultiVariateKraskov implements
 	 */
 	public abstract double computeAverageLocalOfObservations(int[] reordering) throws Exception;
 
-	/**
-	 * Compute the significance of the mutual information of the previously supplied observations.
-	 * We destroy the p(x,y) correlations, while retaining the p(x), p(y) marginals, to check how
-	 *  significant this mutual information actually was.
-	 *  
-	 * This is in the spirit of Chavez et. al., "Statistical assessment of nonlinear causality:
-	 *  application to epileptic EEG signals", Journal of Neuroscience Methods 124 (2003) 113-128
-	 *  which was performed for Transfer entropy.
-	 * 
-	 * @param numPermutationsToCheck
-	 * @return the proportion of MI scores from the distribution which have higher or equal MIs to ours.
-	 */
-	public synchronized EmpiricalMeasurementDistribution computeSignificance(int numPermutationsToCheck) throws Exception {
-		// Generate the re-ordered indices:
-		RandomGenerator rg = new RandomGenerator();
-		// (Not necessary to check for distinct random perturbations)
-		int[][] newOrderings = rg.generateRandomPerturbations(data1.length, numPermutationsToCheck);
-		return computeSignificance(newOrderings);
-	}
-	
-	/**
-	 * Compute the significance of the mutual information of the previously supplied observations.
-	 * We destroy the p(x,y) correlations, while retaining the p(x), p(y) marginals, to check how
-	 *  significant this mutual information actually was.
-	 *  
-	 * This is in the spirit of Chavez et. al., "Statistical assessment of nonlinear causality:
-	 *  application to epileptic EEG signals", Journal of Neuroscience Methods 124 (2003) 113-128
-	 *  which was performed for Transfer entropy.
-	 * 
-	 * @param newOrderings the specific new orderings to use
-	 * @return the proportion of MI scores from the distribution which have higher or equal MIs to ours.
-	 */
-	public EmpiricalMeasurementDistribution computeSignificance(int[][] newOrderings) throws Exception {
-		int numPermutationsToCheck = newOrderings.length;
-		if (!miComputed) {
-			computeAverageLocalOfObservations();
-		}
-		// Store the real observations and their MI:
-		double actualMI = mi;
-		
-		EmpiricalMeasurementDistribution measDistribution = new EmpiricalMeasurementDistribution(numPermutationsToCheck);
-		
-		int countWhereMiIsMoreSignificantThanOriginal = 0;
-		for (int i = 0; i < numPermutationsToCheck; i++) {
-			// Compute the MI under this reordering
-			double newMI = computeAverageLocalOfObservations(newOrderings[i]);
-			measDistribution.distribution[i] = newMI;
-			if (debug){
-				System.out.println("New MI was " + newMI);
-			}
-			if (newMI >= actualMI) {
-				countWhereMiIsMoreSignificantThanOriginal++;
-			}
-		}
-		
-		// Restore the actual MI and the observations
-		mi = actualMI;
-
-		// And return the significance
-		measDistribution.pValue = (double) countWhereMiIsMoreSignificantThanOriginal / (double) numPermutationsToCheck;
-		measDistribution.actualValue = mi;
-		return measDistribution;
-	}
-
 	public abstract double[] computeLocalOfPreviousObservations() throws Exception;
 
 	public double[] computeLocalUsingPreviousObservations(double[][] states1, double[][] states2) throws Exception {
@@ -257,21 +171,5 @@ public abstract class MutualInfoCalculatorMultiVariateKraskov implements
 		throw new Exception("Local method not implemented yet");
 	}
 	
-	public abstract String printConstants(int N) throws Exception ;
-
-	public void setDebug(boolean debug) {
-		this.debug = debug;
-	}
-
-	public double getLastAverage() {
-		return mi;
-	}
-	
-	public int getNumObservations() {
-		return data1.length;
-	}
-
-	public boolean getAddedMoreThanOneObservationSet() {
-		return addedMoreThanOneObservationSet;
-	}
+	public abstract String printConstants(int N) throws Exception ;	
 }
