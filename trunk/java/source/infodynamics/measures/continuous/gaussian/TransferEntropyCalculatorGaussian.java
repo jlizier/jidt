100d98fbd7709c018bc407223f851044863fd051
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2014-04-23 02:07:29 +0000

    Added methods for computeSignificance analytically (i.e. without bootstrapping) to the new Gaussian TE calculators (i.e. the Granger causality calculators), plus for directly assigning the joint covariance matrix for each calculator, and a debug method to access the computed determinants.

--- trunk/java/source/infodynamics/measures/continuous/gaussian/TransferEntropyCalculatorGaussian.java
+++ trunk/java/source/infodynamics/measures/continuous/gaussian/TransferEntropyCalculatorGaussian.java
@@ -1,6 +1,8 @@
 package infodynamics.measures.continuous.gaussian;
 
 import infodynamics.measures.continuous.TransferEntropyCalculatorViaCondMutualInfo;
+import infodynamics.utils.AnalyticNullDistributionComputer;
+import infodynamics.utils.ChiSquareMeasurementDistribution;
 
 /**
  * 
@@ -40,7 +42,8 @@ import infodynamics.measures.continuous.TransferEntropyCalculatorViaCondMutualIn
  *
  */
 public class TransferEntropyCalculatorGaussian
-	extends TransferEntropyCalculatorViaCondMutualInfo {
+	extends TransferEntropyCalculatorViaCondMutualInfo 
+	implements AnalyticNullDistributionComputer {
 	
 	public static final String COND_MI_CALCULATOR_GAUSSIAN = ConditionalMutualInfoCalculatorMultiVariateGaussian.class.getName();
 	
@@ -53,5 +56,63 @@ public class TransferEntropyCalculatorGaussian
 	 */
 	public TransferEntropyCalculatorGaussian() throws InstantiationException, IllegalAccessException, ClassNotFoundException {
 		super(COND_MI_CALCULATOR_GAUSSIAN);
+	}
+
+	/**
+	 * <p>Set the joint covariance of the distribution for which we will compute the
+	 *  transfer entropy.</p>
+	 *  
+	 * <p>Note that without setting any observations, you cannot later
+	 *  call {@link #computeLocalOfPreviousObservations()}, and without
+	 *  providing the means of the variables, you cannot later call
+	 *  {@link #computeLocalUsingPreviousObservations(double[][], double[][])}.</p>
+	 * 
+	 * @param covariance joint covariance matrix of source, dest, dest history
+	 *  variables, considered together.
+	 * @param numObservations the number of observations that the covariance
+	 *  was determined from. This is used for later significance calculations
+	 * @throws Exception for covariance matrix not matching the expected dimensions,
+	 *  being non-square, asymmetric or non-positive definite
+	 */
+	public void setCovariance(double[][] covariance, int numObservations) throws Exception {
+		((ConditionalMutualInfoCalculatorMultiVariateGaussian) condMiCalc).
+				setCovariance(covariance, numObservations);
+	}
+
+	/**
+	 * <p>Compute the statistical significance of the TE 
+	 *  result analytically, without creating a distribution
+	 *  under the null hypothesis by bootstrapping.
+	 *  Computed using the corresponding method of the
+	 *  underlying 
+	 *  {@link ConditionalMutualInfoCalculatorMultiVariateGaussian}</p>
+	 *  
+	 * @see {@link ConditionalMutualInfoCalculatorMultiVariateGaussian#computeSignificance()}
+	 * @return ChiSquareMeasurementDistribution object 
+	 *  This object contains the proportion of TE scores from the distribution
+	 *  which have higher or equal TEs to ours.
+	 */
+	public ChiSquareMeasurementDistribution computeSignificance()
+			throws Exception {
+		return ((ConditionalMutualInfoCalculatorMultiVariateGaussian) condMiCalc).computeSignificance();
+	}
+	
+	/**
+	 * Debug method to check the computed determinants
+	 * 
+	 * @return an array of the four relevant determinants
+	 *  computed by the underlying {@link ConditionalMutualInfoCalculatorMultiVariateGaussian}:
+	 *  of the whole covariance matrix, of the source and conditionals,
+	 *  of the destination and conditionals, and of the conditionals themselves.
+	 */
+	public double[] getDeterminants() {
+		ConditionalMutualInfoCalculatorMultiVariateGaussian condMiGaussian = 
+				(ConditionalMutualInfoCalculatorMultiVariateGaussian) condMiCalc;
+		double[] determinants = new double[4];
+		determinants[0] = condMiGaussian.detCovariance;
+		determinants[1] = condMiGaussian.det1cCovariance;
+		determinants[2] = condMiGaussian.det2cCovariance;
+		determinants[3] = condMiGaussian.detccCovariance;
+		return determinants;
 	}
 }
