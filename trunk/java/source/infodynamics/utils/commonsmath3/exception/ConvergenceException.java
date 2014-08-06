655de8b01a010ec538fb3d3f3917ceac7b1eb0a5
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2014-08-06 05:31:37 +0000

    Adding GPLv3 license statement to all code headers in the infodymamics.measures.continuous package (for starters)

--- trunk/java/source/infodynamics/utils/commonsmath3/exception/ConvergenceException.java
+++ trunk/java/source/infodynamics/utils/commonsmath3/exception/ConvergenceException.java
@@ -14,7 +14,7 @@
  *  
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 /*
  * This class was originally distributed as part of the Apache Commons
