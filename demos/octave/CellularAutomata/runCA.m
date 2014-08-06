e6242a544c9590a8ad2c2e2beae49d2e85f28619
joseph.lizier <joseph.lizier@gmail.com>
2014-08-06 06:52:59 +0000

    Adding GPLv3 license statement to all code headers in the demos; also adding readme for use of octave-java code; correcting references to papers from CA code

--- demos/octave/CellularAutomata/runCA.m
+++ demos/octave/CellularAutomata/runCA.m
@@ -1,8 +1,23 @@
+%%
+%%  Java Information Dynamics Toolkit (JIDT)
+%%  Copyright (C) 2012, Joseph T. Lizier
+%%  
+%%  This program is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%  
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%  
+%%  You should have received a copy of the GNU General Public License
+%%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
+
 % function [caStates, ruleTable, executedRules] = runCA(neighbourhood, base, rule, cells, steps, debug, seedOrState)
 %
-% Joseph Lizier
-% 2012
-% Distributed under GPLv3 (see distribution of license with the code)
 %
 % Please cite:
 % Joseph T. Lizier, "JIDT: An information-theoretic toolkit for studying the dynamics of complex systems", 2012, https://code.google.com/p/information-dynamics-toolkit/
--- demos/octave/DetectingInteractionLags/coupledLogisticMap.m
+++ demos/octave/DetectingInteractionLags/coupledLogisticMap.m
@@ -1,3 +1,21 @@
+%%
+%%  Java Information Dynamics Toolkit (JIDT)
+%%  Copyright (C) 2012, Joseph T. Lizier
+%%  
+%%  This program is free software: you can redistribute it and/or modify
+%%  it under the terms of the GNU General Public License as published by
+%%  the Free Software Foundation, either version 3 of the License, or
+%%  (at your option) any later version.
+%%  
+%%  This program is distributed in the hope that it will be useful,
+%%  but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%  GNU General Public License for more details.
+%%  
+%%  You should have received a copy of the GNU General Public License
+%%  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+%%
+
 % Examine transfer entropy from variable X to Y for coupled logistic maps X and Y
 %  where the process is as defined in Example V.A of:
 %  Pompe and Runge, Momentary information transfer as a coupling measure of time series,
