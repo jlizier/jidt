a16d5fbec47df923881cbd3e0704686429239780
joseph.lizier@gmail.com <joseph.lizier@gmail.com@a5dbf834-a34d-c3bd-19d4-a43a1674ff83>
2014-08-06 05:20:17 +0000

    Adding notices about derived works from the JAMA project to source files

--- trunk/java/source/infodynamics/utils/MatrixUtils.java
+++ trunk/java/source/infodynamics/utils/MatrixUtils.java
@@ -3463,7 +3463,27 @@ public class MatrixUtils {
 
 		return result; 
 	}
-		
+	
+	/*
+	 * The method CholeskyDecomposition() was adapted from the 
+	 *  JAMA project -- http://math.nist.gov/javanumerics/jama/
+	 *  
+	 * This code was distributed with the following original license:
+	 * 
+	 * Copyright Notice
+	 * 
+	 * This software is a cooperative product of The MathWorks and the
+	 * National Institute of Standards and Technology (NIST) which has
+	 * been released to the public domain. Neither The MathWorks nor
+	 * NIST assumes any responsibility whatsoever for its use by other
+	 * parties, and makes no guarantees, expressed or implied, about
+	 * its quality, reliability, or any other characteristic.
+	 * 
+	 * As Jama is in the public domain other developers are free to
+	 * adopt and adapt this code to other styles of programming or to
+	 * extend or modernize the API.
+	 * Make note, however, that NIST makes no endorsement of these projects.
+	 */
 	/**
 	 * <p>Make the Cholesky decomposition L of a given input matrix A,
 	 * where:
@@ -3551,6 +3571,12 @@ public class MatrixUtils {
 	}
 	// TODO implement solve for identity matrix
 	
+	/*
+	 * The method solveViaCholeskyResult() was adapted from the 
+	 *  JAMA project -- http://math.nist.gov/javanumerics/jama/
+	 *  
+	 * See license above the CholeskyDecomposition() method
+	 */
 	/**
 	 * <p>Solve A*X = B, where A = L*L^T via Cholesky decomposition.
 	 * </p>
