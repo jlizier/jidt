e7b8958fdac60b5d84ee0af263b683949f0e16de
joseph.lizier <joseph.lizier@gmail.com>
2014-08-13 15:36:10 +0000

    Adding package-info.java files for Javadocs for the various packages

---
+++ java/source/infodynamics/measures/continuous/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * This package contains Java Interfaces for each of the
+ * information dynamics measures as applied to continuous data
+ * (i.e. double[] or double[][] data).
+ * 
+ * <p>Subpackages provides various estimator types which implement
+ * these interfaces.</p>
+ * 
+ * @author Joseph Lizier (<a href="joseph.lizier at gmail.com">email</a>,
+ * <a href="http://lizier.me/joseph/">www</a>)
+ */
+package infodynamics.measures.continuous;
