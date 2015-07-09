ed34c39660d60a336d277c8f8699dff0d9baac7e
joseph.lizier <joseph.lizier@gmail.com>
2015-07-09 14:46:30 +0000

    Had to correct the auto analyser scripts for TE to call TE specifically again

--- demos/AutoAnalyser/runTEAutoAnalyser.bat
+++ demos/AutoAnalyser/runTEAutoAnalyser.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 
 REM Make sure the latest example source file is compiled.
-javac -classpath "..\java;..\..\infodynamics.jar" "..\java\infodynamics\demos\autoanalysis\AutoAnalyser.java"
+javac -classpath "..\java;..\..\infodynamics.jar" "..\java\infodynamics\demos\autoanalysis\AutoAnalyserTE.java"
 
 # Run the example:
-java -classpath "..\java;..\..\infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyser
+java -classpath "..\java;..\..\infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyserTE
 
