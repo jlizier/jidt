8675858e982405b576e397e8d97c5c9057f6c43c
Pedro Martinez Mediano <pedro.martinez-mediano13@imperial.ac.uk>
2017-05-19 02:16:15 +1000

    Moarr tests.

--- java/source/infodynamics/measures/continuous/kraskov/cuda/unittests.cpp
+++ java/source/infodynamics/measures/continuous/kraskov/cuda/unittests.cpp
@@ -21,10 +21,13 @@ CASE("Basic kNN test")
   int nchunks = 1;
   int indexes[4];
   float distances[4];
-  float pointset[8] = {-1, 0.5, 1.1, 2, -1, 0.5, 1.1, 2};
+  float pointset[8] = {-1, 0.5, 1.1, 2,
+                       -1, 0.5, 1.1, 2};
 
-  cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
-      nchunks, dims, N, useMaxNorm);
+  int err = cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
+              nchunks, dims, N, useMaxNorm);
+
+  EXPECT(err == 1);
 
   EXPECT(indexes[0] == 1);
   EXPECT(indexes[1] == 2);
@@ -48,10 +51,13 @@ CASE("Basic kNN test with L2 norm")
   int nchunks = 1;
   int indexes[4];
   float distances[4];
-  float pointset[8] = {-1, 0.5, 1.1, 2, -1, 0.5, 1.1, 2};
+  float pointset[8] = {-1, 0.5, 1.1, 2,
+                       -1, 0.5, 1.1, 2};
 
-  cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
-      nchunks, dims, N, useMaxNorm);
+  int err = cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
+              nchunks, dims, N, useMaxNorm);
+
+  EXPECT(err == 1);
 
   EXPECT(indexes[0] == 1);
   EXPECT(indexes[1] == 2);
@@ -65,20 +71,95 @@ CASE("Basic kNN test with L2 norm")
 
 },
 
-  //---------------------------------------------
-  // Test kNN with more points far away
-  
+CASE("Test kNN test with longer sequences")
+{
+  int thelier = 0;
+  int useMaxNorm = 0;
+  int N = 10;
+  int dims = 2;
+  int k = 1;
+  int nchunks = 1;
+  int indexes[10];
+  float distances[10];
+  float pointset[20] = {-1, 0.5, 1.1, 2, 10, 11, 10.5, -100, -50, 666,
+                        -1, 0.5, 1.1, 2, 98, -9, -200, 45.3, -53, 0.1};
 
-  //---------------------------------------------
-  // Test kNN 
+  int err = cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
+              nchunks, dims, N, useMaxNorm);
+
+  EXPECT(err == 1);
+
+  EXPECT(indexes[0] == 1);
+  EXPECT(indexes[1] == 2);
+  EXPECT(indexes[2] == 1);
+  EXPECT(indexes[3] == 2);
+
+  EXPECT(distances[0] == approx(4.5));
+  EXPECT(distances[1] == approx(0.72));
+  EXPECT(distances[2] == approx(0.72));
+  EXPECT(distances[3] == approx(1.62));
+
+},
+
+CASE("Smoke test with big random dataset")
+{
+  int thelier = 0;
+  int useMaxNorm = 1;
+  int N = 1000;
+  int dims = 5;
+  int k = 4;
+  int nchunks = 1;
+  int *indexes = (int *) malloc(N * k * sizeof(int));
+  float *distances = (float *) malloc(N * k * sizeof(float));
+  float *pointset = (float *) malloc(N * dims * sizeof(float));
+
+  int err = cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
+              nchunks, dims, N, useMaxNorm);
+
+  free(indexes); free(distances); free(pointset);
+
+  EXPECT(err == 1);
+
+},
+
+CASE("Test with multiple trials in the same call")
+{
+  int thelier = 0;
+  int useMaxNorm = 1;
+  int N = 8;
+  int dims = 2;
+  int k = 1;
+  int nchunks = 2;
+  int indexes[8];
+  float distances[8];
+  float pointset[16] = {-1, 0.5, 1.1, 2, -1.1, -2, -1.6, -1,
+                        -1, 0.5, 1.1, 2,   -1, -2, -1.6, -1};
+
+  int err = cudaFindKnn(indexes, distances, pointset, pointset, k, thelier,
+              nchunks, dims, N, useMaxNorm);
+
+  EXPECT(err == 1);
+
+  EXPECT(indexes[0] == 1);
+  EXPECT(indexes[1] == 2);
+  EXPECT(indexes[2] == 1);
+  EXPECT(indexes[3] == 2);
+  EXPECT(indexes[4] == 3);
+  EXPECT(indexes[5] == 2);
+  EXPECT(indexes[6] == 1);
+  EXPECT(indexes[7] == 0);
+
+  EXPECT(distances[0] == approx(1.5));
+  EXPECT(distances[1] == approx(0.6));
+  EXPECT(distances[2] == approx(0.6));
+  EXPECT(distances[3] == approx(0.9));
+  EXPECT(distances[4] == approx(0.1));
+  EXPECT(distances[5] == approx(0.4));
+  EXPECT(distances[6] == approx(0.4));
+  EXPECT(distances[7] == approx(0.1));
 
 
-  //---------------------------------------------
-  // Test kNN in higher dimensions, padding with zeros
-
-
-  //---------------------------------------------
-  // Test kNN in higher dimensions, padding with zeros
+},
 
 };
 
