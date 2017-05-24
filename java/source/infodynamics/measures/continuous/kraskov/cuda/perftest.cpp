7d9d7ad1359649fbad4f5b6367829e084d5ac5a6
Pedro Martinez Mediano <pedro.martinez-mediano13@imperial.ac.uk>
2017-05-24 15:48:19 +1000

    Added MI calculation script for benchmarking purposes.

---
+++ java/source/infodynamics/measures/continuous/kraskov/cuda/perftest.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <random>
+#include <chrono>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "gpuKnnLibrary.h"
+#include "gpuMILibrary.h"
+#include "digamma.h"
+#include "ctimer.h"
+
+int main(int argc, char *argv[]){
+  std::default_random_engine generator;
+  std::normal_distribution<double> distribution(0, 1.0);
+
+  int N = 1000;
+  int dimx = 1;
+  int dimy = 1;
+  int k = 4;
+  int thelier = 0;
+  int nb_surrogates = 0;
+  int returnLocals = 0;
+  int useMaxNorm = 1;
+  int isAlgorithm1 = 1;
+  int resultSize;
+
+  if (argc > 1) {
+    nb_surrogates = atoi(argv[1]);
+  }
+
+  CPerfTimer pt1 = startTimer("Time generating data");
+  // float source[5] = {0.1, 0.2, 0.3, 0.4, 0.5};
+  // float dest[5] = {0.11, 0.22, 0.31, 0.42, 0.51};
+
+  float *source = (float *) malloc(N*dimx*sizeof(float));
+  float *dest = (float *) malloc(N*dimy*sizeof(float));
+
+  for (int i = 0; i < N*dimx; ++i) {
+    source[i] = distribution(generator);
+  }
+  for (int i = 0; i < N*dimy; ++i) {
+    dest[i] = distribution(generator) + source[i];
+  }
+
+  if (nb_surrogates == 0) {
+    resultSize = 3;
+  } else {
+    resultSize = nb_surrogates + 1;
+  }
+  float result[resultSize];
+  stopTimer(pt1);
+
+  pt1 = startTimer("Time for full calculation");
+  (void) MIKraskov_C(N, source, dimx, dest, dimy, k, thelier,
+      nb_surrogates, returnLocals, useMaxNorm, isAlgorithm1, result);
+  stopTimer(pt1);
+
+  free(source);
+  free(dest);
+
+  for (int i = 0; i < resultSize; i++) {
+    std::cout << result[i] << "\n";
+  }
+
+  return 0;
+}
+
