c9d6a8a3bcd85f5a8cc931fefb00b36b832efbcb
Pedro Martinez Mediano <pedro.martinez-mediano13@imperial.ac.uk>
2017-05-30 17:39:06 +1000

    Now Makefile finds compute capabilities of all GPUs in the system.

--- java/source/infodynamics/measures/continuous/kraskov/cuda/findComputeCapability.cu
+++ java/source/infodynamics/measures/continuous/kraskov/cuda/findComputeCapability.cu
@@ -3,10 +3,16 @@ int main() {
   int count = 0;
   if (cudaSuccess != cudaGetDeviceCount(&count)) return -1;
   if (count == 0) return -1;
-  for (int device = 0; device < count; ++device) {
+  if (count == 1) {
     cudaDeviceProp prop;
-    if (cudaSuccess == cudaGetDeviceProperties(&prop, device)) {
-      std::printf("%d%d", prop.major, prop.minor);
+    cudaGetDeviceProperties(&prop, 0);
+    std::printf("-arch=sm_%d%d", prop.major, prop.minor);
+  } else {
+    for (int device = 0; device < count; ++device) {
+      cudaDeviceProp prop;
+      if (cudaSuccess == cudaGetDeviceProperties(&prop, device)) {
+        std::printf("-gencode arch=compute_%d%d,code=sm_%d%d ", prop.major, prop.minor, prop.major, prop.minor);
+      }
     }
   }
   return 0;
