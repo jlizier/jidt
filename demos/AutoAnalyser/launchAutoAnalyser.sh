4fe82359ff052444609ce7f033bb691d05ad649a
jlizier <joseph.lizier@gmail.com>
2017-11-24 21:53:21 +1100

    Fixing launch location of where the AutoAnalyserLauncher GUI is run from by its launcher shell scripts (to be the JIDT root)

--- demos/AutoAnalyser/launchAutoAnalyser.sh
+++ demos/AutoAnalyser/launchAutoAnalyser.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Need to change directory upwards since the launcher expects to run from JIDT root
+cd ../..
+
 # Run the AutoAnalyser Launcher:
 java -classpath "../../infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyserLauncher
 
