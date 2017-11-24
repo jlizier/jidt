326e5fbe774badddf05da0ebd2a5ebb39b7f13de
jlizier <joseph.lizier@gmail.com>
2017-11-24 21:41:30 +1100

    AutoAnalyserLauncher shell scripts added (this is a new GUI to start all AutoAnalysers from, which was added two commits ago (but I forgot to comment in the message that it was added!)

---
+++ demos/AutoAnalyser/launchAutoAnalyser.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+
+REM Run the AutoAnalyser launcher:
+java -classpath "..\..\infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyserLauncher
+
---
+++ demos/AutoAnalyser/launchAutoAnalyser.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Run the AutoAnalyser Launcher:
+java -classpath "../../infodynamics.jar" infodynamics.demos.autoanalysis.AutoAnalyserLauncher
+
