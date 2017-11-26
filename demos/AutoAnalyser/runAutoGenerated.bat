f902aa69dd3a6423d887ef73e703b0fc5eda221f
jlizier <joseph.lizier@gmail.com>
2017-11-27 09:42:30 +1100

    Fixed comment character in bat file to run the auto generated code

--- demos/AutoAnalyser/runAutoGenerated.bat
+++ demos/AutoAnalyser/runAutoGenerated.bat
@@ -3,6 +3,6 @@
 REM Make sure the latest example source file is compiled.
 javac -classpath "..\java;..\..\infodynamics.jar" "..\java\infodynamics\demos\autoanalysis\GeneratedCalculator.java"
 
-# Run the example:
+REM Run the example:
 java -classpath "..\java;..\..\infodynamics.jar" infodynamics.demos.autoanalysis.GeneratedCalculator
 
